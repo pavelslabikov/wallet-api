b9b2a1d3e2d60ee0680577de445b75990031e37f
Pashka <pslabikov@mail.ru>
2021-01-06 15:08:32 +0500

    Added equals + hashcode realization

--- src/main/java/models/Account.java
+++ src/main/java/models/Account.java
@@ -1,22 +1,51 @@
 package models;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Account {
     private final List<Transaction> transactions;
     private final int number;
+    private float balance;
 
-    public Account(int number, List<Transaction> transactions) {
-        this.transactions = Collections.unmodifiableList(transactions);
+    public Account(int number, float balance) {
+        this.transactions = new ArrayList<>();
         this.number = number;
+        this.balance = balance;
     }
 
-    public List<Transaction> getTransactions() {
-        return transactions;
+    public void addTransaction(Transaction transaction) {
+        if (transaction == null)
+            throw new IllegalArgumentException("Transaction cannot be null");
+        transactions.add(transaction);
+
+        if (transaction.getType() == TransactionType.INCOME)
+            balance += transaction.getAmount();
+        else
+            balance -= transaction.getAmount();
     }
 
     public int getNumber() {
         return number;
+    }
+
+    public float getBalance() {
+        return balance;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Account account = (Account) o;
+        return number == account.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
     }
 }
--- src/main/java/models/Transaction.java
+++ src/main/java/models/Transaction.java
@@ -1,31 +1,25 @@
 package models;
 
 import java.util.Date;
+import java.util.Objects;
 
 public class Transaction {
-    private final int id;
     private final TransactionType type;
     private final Date date;
     private final float amount;
     private String description = "";
 
-    public Transaction(int id, TransactionType type, Date date, float amount, String description) {
-        this.id = id;
-        this.type = type;
+    public Transaction(String type, Date date, float amount, String description) {
+        this.type = TransactionType.valueOf(type);
         this.date = date;
         this.amount = amount;
         this.description = description;
     }
 
-    public Transaction(int id, TransactionType type, Date date, float amount) {
-        this.id = id;
-        this.type = type;
+    public Transaction(String type, Date date, float amount) {
+        this.type = TransactionType.valueOf(type);
         this.date = date;
         this.amount = amount;
-    }
-
-    public int getId() {
-        return id;
     }
 
     public TransactionType getType() {
@@ -42,5 +36,23 @@ public class Transaction {
 
     public String getDescription() {
         return description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Transaction that = (Transaction) o;
+        return Float.compare(that.amount, amount) == 0 &&
+                type == that.type &&
+                date.equals(that.date) &&
+                description.equals(that.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(type, date, amount, description);
     }
 }
