524f74d4b05446138114045df3aba8af2dd83a50
Pashka <pslabikov@mail.ru>
2021-01-08 14:07:03 +0500

    Modified exception classes

--- src/main/java/exceptions/AccountAlreadyExistException.java
+++ src/main/java/exceptions/AccountAlreadyExistException.java
@@ -1,2 +1,9 @@
-package exceptions;public class AccountAlreadyExistException {
+package exceptions;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value= HttpStatus.METHOD_NOT_ALLOWED, reason="Account already exist")
+public class AccountAlreadyExistException extends RuntimeException {
 }
