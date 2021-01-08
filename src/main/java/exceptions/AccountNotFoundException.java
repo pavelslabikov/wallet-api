59ccfb8caaf206cba1b1f6ad9275725dad2b9c3a
Pashka <pslabikov@mail.ru>
2021-01-08 14:07:03 +0500

    Modified exception classes

--- src/main/java/exceptions/AccountNotFoundException.java
+++ src/main/java/exceptions/AccountNotFoundException.java
@@ -1,2 +1,8 @@
-package exceptions;public class AccountNotFoundException {
+package exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value= HttpStatus.NOT_FOUND, reason="No such account")
+public class AccountNotFoundException extends RuntimeException {
 }
