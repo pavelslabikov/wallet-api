5da52057db37e0bf0ac142893f3729da16a65d59
Pashka <pslabikov@mail.ru>
2021-01-15 20:28:30 +0500

    Added beans + refactoring

--- src/main/java/core/config/WebConfig.java
+++ src/main/java/core/config/WebConfig.java
@@ -1,10 +1,20 @@
 package core.config;
 
 import core.dao.AccountDao;
+import core.dao.TransactionDao;
 import core.dao.simple.SimpleAccountDao;
+import core.dao.sql.SqlAccountDao;
+import core.dao.sql.SqlTransactionDao;
 import core.models.Account;
+import org.postgresql.ds.PGConnectionPoolDataSource;
+import org.postgresql.ds.PGSimpleDataSource;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.ConnectionPoolDataSource;
+import javax.sql.DataSource;
 
 import java.util.HashSet;
 
@@ -12,11 +22,27 @@ import java.util.HashSet;
 public class WebConfig {
     @Bean
     public AccountDao accountDao() {
-        return new SimpleAccountDao(accounts());
+        return new SqlAccountDao(template());
+    }
+
+    @Bean
+    public TransactionDao transactionDao() {
+        return new SqlTransactionDao(template());
     }
 
     @Bean
     public HashSet<Account> accounts() {
         return new HashSet<>();
+    }
+
+    @Bean
+    public JdbcTemplate template() {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.type(PGSimpleDataSource.class);
+        dataSourceBuilder.driverClassName("org.postgresql.Driver");
+        dataSourceBuilder.url("jdbc:postgresql://localhost:5432/accounts");
+        dataSourceBuilder.username("postgres");
+        dataSourceBuilder.password("123");
+        return new JdbcTemplate(dataSourceBuilder.build());
     }
 }
