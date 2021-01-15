8ed178d57ab88d45c1a08c2608998133cbaa2104
Pashka <pslabikov@mail.ru>
2021-01-15 20:24:36 +0500

    Created SQL-based account DAO

--- src/main/java/core/dao/AccountDao.java
+++ src/main/java/core/dao/AccountDao.java
@@ -1,14 +1,14 @@
 package core.dao;
 
 import core.models.Account;
-import org.springframework.lang.Nullable;
 
+import java.util.List;
 import java.util.Set;
 
 public interface AccountDao {
     void createAccount(Account account);
     void deleteAccountByNumber(int number);
-    @Nullable Account getByNumber(int number);
-    Set<Account> getAllAccounts();
-    boolean hasAccount(Account account);
+    Account getByNumber(int number);
+    List<Account> getAllAccounts();
+    void updateAccount(Account account);
 }
