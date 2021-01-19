d4fb8739fc2425418489e55ff63ceb8ca7433c40
Pashka <pslabikov@mail.ru>
2021-01-19 17:01:58 +0500

    Filled controller test classes with test cases

--- src/test/java/core/TestAccountController.java
+++ src/test/java/core/TestAccountController.java
@@ -1,49 +1,88 @@
-import core.controllers.AccountController;
+package core;
+
 import core.dao.AccountDao;
-import core.dao.simple.SimpleAccountDao;
 import core.models.Account;
-import org.checkerframework.checker.units.qual.A;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-
-import java.util.Collections;
-import java.util.HashSet;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class TestAccountController {
+    @LocalServerPort
+    private int port;
+
+    private String resourceUrl;
+
     @Autowired
     private TestRestTemplate restTemplate;
 
     @MockBean(name = "sqlAccountDao")
     private AccountDao accountDao;
 
-    @Test
-    public void testReceivingNotFoundResponse() {
+    @Before
+    public void setUpBeforeTest() {
         MockitoAnnotations.initMocks(this);
-        var testAccount = new Account(1, 2);
-        when(accountDao.getByNumber(2)).thenReturn(testAccount);
-        var actualAccount = restTemplate.getForEntity("http://localhost:8080/v1/accounts/2", Account.class);
-        assert actualAccount.getStatusCode() == HttpStatus.OK;
+        resourceUrl = String.format("http://localhost:%d/v1/accounts/", port);
+    }
 
+    @Test
+    public void testGettingNonExistingAccount() {
+        mockNonExistingAccount();
+        var response = restTemplate.getForEntity(resourceUrl + "1", Object.class);
+        Assert.assertEquals(response.getStatusCode(), HttpStatus.NOT_FOUND);
+    }
+
+    @Test
+    public void testGettingExistingAccount() {
+        var testAccount = new Account(2, 2);
+        mockExistingAccount(testAccount);
+        var response = restTemplate.getForEntity(resourceUrl + "2", Account.class);
+        Assert.assertEquals(response.getStatusCode(), HttpStatus.OK);
+        Assert.assertEquals(testAccount, response.getBody());
+    }
+
+    @Test
+    public void testGettingAllAccounts() {
+        var testAccounts = new Account[] {
+                new Account(1, 1),
+                new Account(2, 2)
+        };
+
+        mockAccountCollection(testAccounts);
+        var response = restTemplate.getForEntity(resourceUrl, Account[].class);
+        Assert.assertEquals(response.getStatusCode(), HttpStatus.OK);
+        Assert.assertArrayEquals(testAccounts, response.getBody());
+    }
+
+    @Test
+    public void testCreatingAlreadyExistingAccount() {
+        var testAccount = new Account(2, 2);
+        mockExistingAccount(testAccount);
+        var response = restTemplate.postForEntity(resourceUrl, testAccount, String.class);
+        Assert.assertEquals(response.getStatusCode(), HttpStatus.METHOD_NOT_ALLOWED);
+    }
+
+    private void mockNonExistingAccount() {
+        when(accountDao.getByNumber(1)).thenReturn(null);
+    }
+
+    private void mockExistingAccount(Account account) {
+        when(accountDao.getByNumber(account.getNumber())).thenReturn(account);
+    }
+
+    private void mockAccountCollection(Account[] accounts) {
+        when(accountDao.getAllAccounts()).thenReturn(accounts);
     }
 }
--- src/test/java/core/TestTransactionController.java
+++ src/test/java/core/TestTransactionController.java
@@ -1,2 +1,91 @@
-package core;public class TestTransactionController {
+package core;
+
+import core.dao.AccountDao;
+import core.dao.TransactionDao;
+import core.models.Account;
+import core.models.Transaction;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Date;
+
+import static org.mockito.Mockito.when;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class TestTransactionController {
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @MockBean(name = "sqlAccountDao")
+    private AccountDao accountDao;
+
+    @MockBean(name = "sqlTransactionDao")
+    private TransactionDao transactionDao;
+
+    @LocalServerPort
+    private int port;
+
+    private String resourceUrl;
+
+    @Before
+    public void setUpBeforeTest() {
+        MockitoAnnotations.initMocks(this);
+        resourceUrl = String.format("http://localhost:%d/v1/accounts/", port);
+    }
+
+    @Test
+    public void testGettingTransactionsFromNonExistingAccount() {
+        mockNonExistingAccount();
+        resourceUrl += "1/transactions";
+        var response = restTemplate.getForEntity(resourceUrl, String.class);
+        Assert.assertEquals(response.getStatusCode(), HttpStatus.NOT_FOUND);
+    }
+
+    @Test
+    public void testCreatingTransactionForNonExistingAccount() {
+        var testTransaction = new Transaction("INCOME", new Date(), 1, "");
+        mockNonExistingAccount();
+        resourceUrl += "1/transactions";
+        var response = restTemplate.postForEntity(resourceUrl, testTransaction, String.class);
+        Assert.assertEquals(response.getStatusCode(), HttpStatus.NOT_FOUND);
+    }
+
+    @Test
+    public void testGettingTransactions() {
+        var testAccount = new Account(2, 2);
+        mockExistingAccount(testAccount);
+        var testTransactions = new Transaction[] {
+                new Transaction("INCOME", new Date(), 1, "")
+        };
+
+        mockTransactionCollection(testAccount.getNumber(), testTransactions);
+        resourceUrl += "2/transactions";
+        var response = restTemplate.getForEntity(resourceUrl, Transaction[].class);
+        Assert.assertEquals(response.getStatusCode(), HttpStatus.OK);
+        Assert.assertArrayEquals(testTransactions, response.getBody());
+    }
+
+    private void mockNonExistingAccount() {
+        when(accountDao.getByNumber(1)).thenReturn(null);
+    }
+
+    private void mockExistingAccount(Account account) {
+        when(accountDao.getByNumber(account.getNumber())).thenReturn(account);
+    }
+
+    private void mockTransactionCollection(int number, Transaction[] transactions) {
+        when(transactionDao.getAllTransactions(number)).thenReturn(transactions);
+    }
 }
